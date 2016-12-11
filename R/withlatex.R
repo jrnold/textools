291400a3a0c43324d5c6b9900cba6c68f3b47b8b
jrnold <jeffrey.arnold@gmail.com>
2016-12-10 21:22:12 -0800

    misc

--- R/withlatex.R
+++ R/withlatex.R
@@ -12,27 +12,34 @@ latex[["X"]] <- as.tex
 
 latex[["x"]] <- tex
 
-# tex arguments
+# tex arguments in [] and {}
 latex[["A"]] <- texarg
-
 latex[["O"]] <- texopt
 
+# Overscript, underscript and fractions
 latex[["%^%"]] <- function(x, y) str_c(x, "^", "{", y, "}")
 latex[["%_%"]] <- function(x, y) str_c(x, "_", "{", y, "}")
 latex[["%/%"]] <- function(x, y) str_c("\\frac{", x, "}{", y, "}")
 
-latex[["m"]] <- math
+# Quotes
+latex[["Q"]] <- function(x) str_c("``", x, "''")
+latex[["q"]] <- function(x) str_c("`", x, "'")
 
+# Math
+latex[["m"]] <- math
 latex[["M"]] <- dmath
 
+# Comments
 latex[["C"]] <- texcomment
 
 # Add tex arguments
 # latex[["%[%"]] <- function(...) brackets(...)
 # latex[["%[[%"]] <- function(...)
 
+# Parentheses. I should make it so {...} %)% ")" and "(" %(% {...} both work.
 latex[["%(%"]] <- brackets
 
+# Newline and continuations
 # combine with no-space
 latex[["%c%"]] <- function(x, y) tex(str_c(x, y, sep = ""))
 # newline
@@ -41,6 +48,7 @@ latex[["%n%"]] <- function(x, y) tex(str_c(x, y, sep = "\n"))
 latex[["%l%"]] <- function(x, y) tex(str_c(x, y, sep = "\\\\\n"))
 # concatenate with space
 latex[["% %"]] <- function(x, y) tex(str_c(x, y, sep = " "))
+latex[["%~%"]] <- function(x, y) tex(str_c(x, y, sep = "~"))
 
 #' @export
 with_latex <- function(code) {
