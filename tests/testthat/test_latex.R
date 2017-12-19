f9173478c5c0ca476cebfcd14cc1e47fea9578cc
jrnold <jeffrey.arnold@gmail.com>
2017-12-19 08:59:00 -0800

    misc

--- tests/testthat/test_latex.R
+++ tests/testthat/test_latex.R
@@ -8,19 +8,19 @@ test_that("tex() works with default args", {
 
 test_that("print.tex produces correct output", {
   x <- tex(c("foo", "\\bar"))
-  expect_equal(capture_output(print(x)), "foo\n\\bar")
+  expect_equal(capture_output(print(x)), "<tex>\nfoo\n\\bar")
 })
 
-test_that("as.tex.tex works as expected", {
-  x <- as.tex(tex("\\foo"))
+test_that("as_tex.tex works as expected", {
+  x <- as_tex(tex("\\foo"))
   expect_is(x, "tex")
   expect_equal(as.character(x), "\\foo")
 })
 
-test_that("as.tex.character escapes by default", {
-  expect_equal(as.tex("$1.00"), tex("\\$1.00"))
+test_that("as_tex.character escapes by default", {
+  expect_equal(as_tex("$1.00"), tex("\\$1.00"))
 })
 
-test_that("as.tex.default works", {
-  expect_equal(as.tex(1), tex("1"))
+test_that("as_tex.default works", {
+  expect_equal(as_tex(1), tex("1"))
 })
